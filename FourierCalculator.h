e95ee0e15f60480a14219fa60389c5613a0ea13a
VikramSGIT <vikram16saminathan@gmail.com>
2023-06-16 07:36:01 +0530

    Added files

---
+++ FourierCalculator.h
@@ -0,0 +1,88 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <complex>
+
+#include <functional>
+#include <unordered_map>
+#include <glm/glm.hpp>
+
+using vertex = glm::vec2;
+using datatype = float;
+using complex = std::complex<datatype>;
+
+// Modified https://rextester.com/discussion/FLHE26043/Romberg-Integration to work with modern c++
+// Romberg's Integration
+template<typename T> std::complex<T> integrateC(const std::function<std::complex<T>(const T&)>& f, const T& a, const T& b, const T& err) {
+    constexpr int max_steps = 20;
+
+    T acc = err != 0 ? err : 1e-10;
+    std::vector<std::vector<std::complex<T>>> R(2, std::vector<std::complex<T>>(max_steps + 1));
+    T cErr = std::numeric_limits<T>().infinity();
+    int cr = 0;
+    T h = b - a;
+
+    R[1 - cr][0] = (f(a) + f(b)) * static_cast<T>(h * 0.5);
+
+    for (int i = 1; i <= max_steps; i++) {
+        h /= 2;
+        std::complex<T> c = 0;
+        int ep = pow(2, (i - 1));
+
+        for (int j = 1; j <= ep; j++)
+            c += f(a + (j * 2 - 1) * h);
+
+        R[cr][0] = c * h + R[1 - cr][0] / static_cast<T>(2);
+
+        for (int j = 1; j <= i; j++) {
+            T n_k = pow<T, T>(4, j);
+            R[cr][j] = (R[cr][j - 1] * static_cast<T>(n_k) - R[1 - cr][j - 1]) / (n_k - 1);
+        }
+
+        cErr = abs(R[1 - cr][i - 1] - R[cr][i]);
+
+        if (i > 1 && cErr < acc)
+            return R[cr][i - 1];
+
+        cr = 1 - cr;
+    }
+
+    return R[1 - cr][max_steps - 1];
+}
+
+void calc(const std::vector<vertex>& xy, uint32_t n, datatype error, std::vector<complex>& Cn) {
+	std::vector<complex> data;
+	auto it = xy.begin();
+
+	float tau = 2 * M_PI;
+
+	for (const vertex& ver : xy) data.push_back({ ver.x, ver.y });
+
+	//f is frequency harmonics
+	for (int64_t f = -static_cast<int64_t>(n); f <= n; f++) {
+		complex C = integrateC<datatype>(std::bind([&data, &f](const datatype& t) -> complex {
+			return data[t * (data.size() - 1)] * std::exp(complex{0, -f * 2 * static_cast<float>(M_PI * t)});
+			}, std::placeholders::_1), 0, 1, error);
+		Cn.push_back(C);
+	}
+}
+
+std::vector<vertex> plot(const std::vector<complex>& Cn, datatype start, datatype end) {
+	std::vector<vertex> res(std::abs(end - start));
+
+	float omega = 2 * M_PI / std::abs(end - start);
+
+    if (start > end) std::swap(start, end);
+
+	for (size_t k = start; k < end; k++) {
+		complex C = { 0, 0 };
+		datatype n_har = -static_cast<float>(Cn.size() / 2);
+		for (const complex& cn : Cn) {
+			C += cn * std::exp(complex{ 0, n_har * omega * k});
+			n_har++;
+		}
+		res[k - start] = vertex{ C.real(), C.imag() };
+	}
+	return res;
+}
---
+++ main.cpp
@@ -0,0 +1,163 @@
+﻿#define MAX_VERTEX 1000
+
+#include "FourierCalculator.h"
+
+#include <iostream>
+
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+void initOpenGLData(uint32_t& vertex_id) {
+    glCreateBuffers(1, &vertex_id);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_id);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex) * MAX_VERTEX, nullptr, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)0);
+}
+
+void initShader(uint32_t& shader) {
+    const char* vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 pos;\n"
+        "uniform mat4 MVP;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = MVP * vec4(pos.x, pos.y, 0.0f, 1.0);\n"
+        "}\0";
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(vec3(1.0f, 1.0f, 1.0f), 1.0f);\n"
+        "}\n\0";
+
+    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    shader = glCreateProgram();
+    glAttachShader(shader, vertexShader);
+    glAttachShader(shader, fragmentShader);
+    glLinkProgram(shader);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+}
+
+int main() {
+    size_t n;
+    std::cout << "Enter value of N (higher the N better the result):" << std::endl;
+    std::cin >> n;
+
+    if (!glfwInit())
+    {
+        std::cerr << "FATAL ERROR: Failed to initialize Opengl" << std::endl;
+        return -1;
+    }
+
+    if (!glfwInit()) {
+        std::cerr << "FATAL ERROR: Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Fourier Transform", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress))
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    struct windowdata {
+        bool mousedown = false;
+        bool drawn = false;
+    } data;
+
+    glfwSetWindowUserPointer(window, &data);
+
+    glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) {
+        if (action == GLFW_PRESS)
+            static_cast<windowdata*>(glfwGetWindowUserPointer(window))->mousedown = true;
+        else if (action == GLFW_RELEASE) {
+            static_cast<windowdata*>(glfwGetWindowUserPointer(window))->mousedown = false;
+            static_cast<windowdata*>(glfwGetWindowUserPointer(window))->drawn = true;
+        }
+        });
+
+    uint32_t shader, vertex_id, res_vertex_id, vertex_count = 0;
+    initShader(shader);
+    initOpenGLData(vertex_id);
+    glfwSwapInterval(1);
+
+    datatype prevtime = glfwGetTime(), polling_rate = 0.01, draw_speed = 0.1;
+    int height, width;
+    glm::mat4 MVP;
+    bool calculated = false;
+
+    std::vector<vertex> vec, res;
+
+    while (!glfwWindowShouldClose(window)) {
+
+        datatype curtime = glfwGetTime();
+
+        if (calculated && curtime - prevtime > draw_speed) {
+            vertex_count++;
+            prevtime = curtime;
+            if (vertex_count >= res.size()) vertex_count = 0;
+        }
+
+        glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        if (data.mousedown && vertex_count < MAX_VERTEX && !data.drawn && curtime - prevtime > polling_rate) {
+            vertex_count++;
+            double x, y;
+            int w_x = 0, w_y = 0;
+            glfwGetCursorPos(window, &x, &y);
+            glfwGetWindowSize(window, &w_x, &w_y);
+            vec.emplace_back(x - w_x/2.0f, -y + w_y/2.0f);
+            glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertex) * vertex_count, sizeof(vertex), &vec.back());
+            prevtime = curtime;
+        }
+
+        glUseProgram(shader);
+
+        glfwGetWindowSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+        MVP = glm::ortho(-(float)width / 2.0f, (float)width / 2.0f, -(float)height / 2.0f, (float)height / 2.0f);
+        glUniformMatrix4fv(glGetUniformLocation(shader, "MVP"), 1, GL_FALSE, glm::value_ptr(MVP));
+
+        glDrawArrays(GL_LINE_STRIP, 1, vertex_count);
+
+        if (data.drawn && !calculated) {
+            float a0;
+            std::vector<std::complex<datatype>> Cn;
+            calc(vec, n, 1, Cn);
+            res = plot(Cn, 1, vec.size());
+
+            calculated = true;
+            vertex_count = 0;
+
+            glBufferData(GL_ARRAY_BUFFER, res.size() * sizeof(vertex), res.begin()._Ptr, GL_STATIC_DRAW);
+        }
+        glDrawArrays(GL_LINE_STRIP, 1, vertex_count);
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteBuffers(1, &vertex_id);
+    if(calculated)  glDeleteBuffers(1, &res_vertex_id);
+
+    glfwTerminate();
+}
